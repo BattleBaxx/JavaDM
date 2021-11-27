1abe63e48a89da4cb2cb9a9da71e34537c33ede2
BattleBaxx <54268057+Ranga1503@users.noreply.github.com>
2021-11-27 19:26:04 +0530

    Added: logic for file end-points

--- JavaDM/src/main/java/api/file/FileController.java
+++ JavaDM/src/main/java/api/file/FileController.java
@@ -1,66 +1,61 @@
 package api.file;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Map;
-
 import core.exceptions.NoSuchFileException;
 import core.util.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FileController {
-	
-	@Autowired
-	private FileService fileService;
-	
-	@RequestMapping("/files")
-	public void getAllFiles() {
-		
-	}
-	
-	@RequestMapping("/file/{id}")
-	public void getFile(@PathVariable String id) {
-		
-	}
 
-	@RequestMapping(method = RequestMethod.GET, value ="/downloadFile/{id}")
-	public ResponseEntity<Resource> getFileDownload(@PathVariable String id) {
-		String fullFilePath = FileUtils.getFullFilePath(id);
+    @Autowired
+    private FileService fileService;
 
-		File respFile = new File(fullFilePath);
+    @RequestMapping("/files")
+    public List<Map<String, String>> getAllFiles() {
+        return fileService.getFiles();
+    }
 
-		if(!respFile.canRead()) {
-			throw new NoSuchFileException("Invalid ID, file does not exist");
-		}
+    @RequestMapping("/file/{id}")
+    public Map<String, String> getFile(@PathVariable String id) {
+        return fileService.getFile(id);
+    }
 
-		FileSystemResource fsr = new FileSystemResource(respFile);
+    @RequestMapping(method = RequestMethod.GET, value = "/downloadFile/{id}")
+    public ResponseEntity<Resource> getFileDownload(@PathVariable String id) {
+        String fullFilePath = FileUtils.getFullFilePath(id);
 
-		return ResponseEntity.ok()
-				.contentLength(respFile.length())
-				.contentType(MediaType.APPLICATION_OCTET_STREAM)
-				.body(fsr);
-	}
-	
-	@RequestMapping(method=RequestMethod.PUT, value="/file/{id}")
-	public void updateFile(@RequestBody Map<String, Object> file, @PathVariable String id) {
-		
-	}
-	
-	@RequestMapping(method=RequestMethod.DELETE, value="/file/{id}")
-	public void deleteFile(@PathVariable String id) {
-		
-	}
-	
-	
+        File respFile = new File(fullFilePath);
+
+        if (!respFile.canRead()) {
+            throw new NoSuchFileException("Invalid ID, file does not exist");
+        }
+
+        FileSystemResource fsr = new FileSystemResource(respFile);
+
+        return ResponseEntity.ok()
+                .contentLength(respFile.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(fsr);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/file/{id}")
+    public void updateFile(@RequestBody Map<String, Object> updatedFileDetails, @PathVariable String id) {
+        fileService.updateFile(updatedFileDetails, id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/file/{id}")
+    public void deleteFile(@PathVariable String id) {
+        fileService.deleteFile(id);
+    }
+
+
 }
--- JavaDM/src/main/java/api/file/FileService.java
+++ JavaDM/src/main/java/api/file/FileService.java
@@ -2,7 +2,58 @@ package api.file;
 
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class FileService {
 
+    private static final DateTimeFormatter DATE_FORMATTER_WITH_TIME = DateTimeFormatter
+            .ofPattern("MMM d, yyyy HH:mm:ss.SSS");
+    final private String downloadPath = System.getenv("downloadFolder");
+
+    public Map<String, String> getFile(String id) {
+        HashMap<String, String> fileDetails = new HashMap<>();
+        Path filepath = new File(downloadPath + id).toPath();
+        BasicFileAttributes attr = null;
+        try {
+            attr = Files.readAttributes(filepath, BasicFileAttributes.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        fileDetails.put("name", id);
+        fileDetails.put("created_time", attr.creationTime().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().format(DATE_FORMATTER_WITH_TIME));
+        fileDetails.put("size", String.valueOf(attr.size()));
+        return fileDetails;
+    }
+
+    public List<Map<String, String>> getFiles() {
+        List<Map<String, String>> files = new ArrayList<>();
+        File directoryPath = new File(downloadPath);
+        String contents[] = directoryPath.list();
+        for (String file : contents) {
+            files.add(getFile(file));
+        }
+        return files;
+    }
+
+    public void deleteFile(String id) {
+        File file = new File(downloadPath + id);
+        file.delete();
+    }
+
+    public void updateFile(Map<String, Object> updatedFileDetails, String id) {
+        File oldFile = new File(downloadPath + id);
+        File newFile = new File(downloadPath + updatedFileDetails.get("name"));
+        oldFile.renameTo(newFile);
+    }
 }
